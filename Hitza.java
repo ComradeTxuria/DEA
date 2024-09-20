7bbeefd6d3b14d408eaa8c34cd1ba69ccc00f427
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-09-20 17:23:30 +0200

    Klasea eta eraikitzailea sortu.

--- Hitza.java
+++ Hitza.java
@@ -1,1 +1,9 @@
+public class Hitza {
+  private String hitza;
+  private ArrayList<WebGunea> webZerrenda;
 
+  public Hitza (String pHitza) {
+    this.hitza = pHitza;
+    this.webZerrenda = new ArrayList<>();
+  }
+}
