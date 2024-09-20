e4e6b021e2e312d9cf0e9edd38f7f9a85d05387f
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2024-09-20 17:05:38 +0200

    Eraikitzailea eta atributuak

--- Nabigatzailea.java
+++ Nabigatzailea.java
@@ -1,1 +1,25 @@
+public class Nabigatzailea
+  {
+    private static Nabigatzailea NireNabigatzailea
+    private ArrayList<Webgunea> wLista;
 
+    private Nabigatzailea()
+    {
+      this.wLista = new ArrayList<Webgunea>();
+    }
+    
+
+    public static Nabigatzailea getNabigatzailea() 
+    {
+		  if (NireNabigatzailea == null) 
+      {
+			  NireNabigatzailea = new Nabigatzailea();
+		  }
+		  return NireNabigatzailea;
+	  }
+
+
+
+
+    
+  }
