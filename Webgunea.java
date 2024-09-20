6b00c5c824a1e5b7d0ae1ba70eaa3ed399423750
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2024-09-20 17:40:02 +0200

    eraikitzaile eta atributuakç+

--- Webgunea.java
+++ Webgunea.java
@@ -1,1 +1,14 @@
+public class Webgunea
+  {
+    private String url;
+    private ArrayList<Webgunea> webguneZerrenda;
+    private ArrayList<Hitza>;
 
+
+    public Webgunea()
+    {
+      this.url = url;
+      this.webguneZerrenda = new ArrayList<Webgune>();
+      this.hitzKlabeZerrenda = new ArrayList<Webgune>();
+    }
+  }
