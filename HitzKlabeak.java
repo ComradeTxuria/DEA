6ccbebb067b934abe9fb97d3b7751b26dd7c979f
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-09-20 17:17:44 +0200

    Klasea sortu, eraikitzailearekin batera.

--- HitzKlabeak.java
+++ HitzKlabeak.java
@@ -1,1 +1,15 @@
+public class HitzKlabeak {
+	private ArrayList<Hitza> hLista;
+	private static HitzKlabeak nireHitzKlabeak = null;
 
+	private HitzKlabeak() {
+		this.hLista = new ArrayList<>();
+	}
+
+	public static HitzKlabeak getHitzKlabeak() {
+		if (nireHitzKlabeak == null) {
+			nireHitzKlabeak = new HitzKlabeak();
+		} 
+	  return nireMazoa;
+	}
+}
