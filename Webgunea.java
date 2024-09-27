0137db7c1f0fc32506f3005b7dd14f1cb58a4c94
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-09-27 11:38:02 +0200

    v0.3

--- Webgunea.java
+++ Webgunea.java
@@ -1,14 +1,23 @@
-public class Webgunea
-  {
-    private String url;
-    private ArrayList<Webgunea> webguneZerrenda;
-    private ArrayList<Hitza> hitzKlabeZerrenda;
+import java.util.ArrayList;
 
+public class Webgunea {
+  private String id;
+  private String url;
+  private ArrayList<Webgunea> webguneZerrenda;
+  private ArrayList<Hitza> hitzKlabeZerrenda;
 
-    public Webgunea()
-    {
-      this.url = url;
-      this.webguneZerrenda = new ArrayList<Webgune>();
-      this.hitzKlabeZerrenda = new ArrayList<Webgune>();
-    }
+  public Webgunea(String url, String id) {
+    this.id = id;
+    this.url = url;
+    this.webguneZerrenda = new ArrayList<>();
+    this.hitzKlabeZerrenda = new ArrayList<>();
   }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+}
