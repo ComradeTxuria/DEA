b10b4b1df5cfc83fb5a9e0a9587a1d5437f3262f
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-09-27 11:37:15 +0200

    v0.1

--- Hitza.java
+++ Hitza.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Hitza {
 	private String hitza;
-	private ArrayList<WebGunea> webZerrenda;
+	private ArrayList<Webgunea> webZerrenda;
 
 	public Hitza (String pHitza) {
 		this.hitza = pHitza;
