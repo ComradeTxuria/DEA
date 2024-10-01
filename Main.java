737040b84a8b1aa335db78c0671de8d150dba912
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-10-01 10:20:37 +0200

    v0.7

--- Main.java
+++ Main.java
@@ -4,26 +4,28 @@ public class Main{
 
     public static void main(String[] args){
         Nabigatzailea nabigatzailea = Nabigatzailea.getNabigatzailea();
+        System.out.println();
+        System.out.println("Webguneak kargatzen ari dira...");
         nabigatzailea.indexIrakurri("index-2024-25");
+        nabigatzailea.lotuWebguneak("pld-arcs-1-N-2024-25");
 
         Scanner input = new Scanner(System.in);
         boolean continueProgram = true;
 
-        while (continueProgram) {
-            System.out.print("\033[H\033[2J");   
-            System.out.flush();
+        while (continueProgram) { 
+            System.out.println();
             System.out.println("___________________________________");
             System.out.println();
             System.out.println("Ongi etorri nabigatzailera.");
             System.out.println();
             System.out.println("1. Web-orri bat bilatu.");
-            System.out.println("2. Web-orri bat txertatu. (WIP)");
-            System.out.println("3. Web-orri bat ezabatu. (WIP)");
+            System.out.println("2. Web-orri bat txertatu.");
+            System.out.println("3. Web-orri bat ezabatu.");
+            System.out.println("4. Web-orri baten lotutako webguneak ikusi.");
             System.out.println();
             System.out.print("Aukeratu nahi duzun eragiketa: ");
             int eragiketa = input.nextInt();
-            System.out.print("\033[H\033[2J");   
-            System.out.flush(); 
+
 
             switch (eragiketa) {
                 case 1 -> {
@@ -36,9 +38,7 @@ public class Main{
                     System.out.println();
                     System.out.print("Aukeratu nahi duzun prozesua: ");
                     int bilatzeProzesua = input.nextInt();
-                    System.out.print("\033[H\033[2J");   
-                    System.out.flush();   
-
+            
 
                     switch (bilatzeProzesua) {
                         case 1 -> {
@@ -46,8 +46,6 @@ public class Main{
                             System.out.println();
                             System.out.print("ID-a idatzi: ");
                             String id = input.next();
-                            System.out.print("\033[H\033[2J");   
-                            System.out.flush();
                             nabigatzailea.webguneaBilatuId(id);
                         }
                         case 2 -> {
@@ -55,8 +53,6 @@ public class Main{
                             System.out.println();
                             System.out.print("Hitz-gakoa idatzi: ");
                             String bilatuHitzGakoa = input.next();
-                            System.out.print("\033[H\033[2J");   
-                            System.out.flush();
                             nabigatzailea.webguneakBilatu(bilatuHitzGakoa);
                         }
 
@@ -64,10 +60,53 @@ public class Main{
 
                 }
                 case 2 -> {
+                    System.out.println("___________________________________");
+                    System.out.println();
+                    System.out.print("Web-orriaren URL-a idatzi: ");
+                    String url = input.next();
+                    System.out.println();
+                    System.out.print("Zer Id erabili nahi duzu?: ");
+                    String ida = input.next();
+                    nabigatzailea.webguneaTxertatu(url, ida);
                 }
                 case 3 -> {
-                }
+                    System.out.println("___________________________________");
+                    System.out.println();
+                    System.out.println("Nola ezabatu nahi duzu web-orria?");
+                    System.out.println();
+                    System.out.println("1. ID baten bitartez.");
+                    System.out.println("2. URL baten bitartez.");
+                    System.out.println();
+                    System.out.print("Aukeratu nahi duzun prozesua: ");
+                    int ezabatuProzesua = input.nextInt();
 
+                    switch (ezabatuProzesua) {
+
+                        case 1-> {
+                            System.out.println("___________________________________");
+                            System.out.println();
+                            System.out.print("ID-a idatzi: ");
+                            String id = input.next();
+                            nabigatzailea.webguneaEzabatu(id);
+
+                        }
+                        case 2 -> {
+                            System.out.println("___________________________________");
+                            System.out.println();
+                            System.out.print("URL-a idatzi: ");
+                            String ezabatuUrl = input.next();
+                            nabigatzailea.webguneaEzabatuUrl(ezabatuUrl);
+                        }
+                    }
+                }
+                case 4 -> {
+                    System.out.println("___________________________________");
+                    System.out.println();
+                    System.out.print("Idatzi web-orriaren URL-a: ");
+                    String url = input.next();
+                    nabigatzailea.webguneLoturikoak(url);
+
+                }
             }
 
             System.out.println("___________________________________");
@@ -81,14 +120,9 @@ public class Main{
             int bueltatu = input.nextInt();
 
             switch (bueltatu) {
-
                 case 1 -> continueProgram = false;
-                case 2 -> {
-                }
-
             }
         }
-
         input.close();
     }
 
