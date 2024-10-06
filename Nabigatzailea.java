2f9e8f0c6064ca5094d173140439748aa67be976
CH3MBER <166596453+CH3MBER@users.noreply.github.com>
2024-10-06 09:44:12 +0200

    v1.0

--- Nabigatzailea.java
+++ Nabigatzailea.java
@@ -12,7 +12,6 @@ public class Nabigatzailea{
     private static Nabigatzailea NireNabigatzailea;
     private final ArrayList<Webgunea> wLista;
  
-    
     private Nabigatzailea(){
       this.wLista = new ArrayList<>();
     }
@@ -23,6 +22,10 @@ public class Nabigatzailea{
 		}
 		return NireNabigatzailea;
 	}
+
+    public ArrayList<Webgunea> getwLista() {
+        return wLista;
+    }
 
     public void indexIrakurri(String dokumentuIzena) {
         try (BufferedReader br = new BufferedReader(new FileReader(dokumentuIzena))) {
@@ -38,7 +41,6 @@ public class Nabigatzailea{
                 }
             }
         } catch (IOException e) {
-            Menua.eragiketakBanatu();
             System.err.println("Errore bat artxiboa irakurtzean:" + e.getMessage());
         }
     }
@@ -67,14 +69,12 @@ public class Nabigatzailea{
                     }
                 }
             }
-        } catch (IOException e) {
-            Menua.eragiketakBanatu();
+        } catch (IOException e) {      
             System.err.println("Errore bat artxiboa irakurtzean:" + e.getMessage());
         }
     }
 
     public void webguneLoturikoak(String id) {
-        Menua.eragiketakBanatu();
         System.out.println("Loturiko web-orriak:");
         System.out.println();
         boolean found = false;
@@ -88,26 +88,21 @@ public class Nabigatzailea{
             }
         }
         if (!found) {
-            Menua.eragiketakBanatu();
             System.out.println("Ez da aurkitu bilatu nahi duzun web-orria.");
         }
     }
 
-    public void webguneakBilatu(String emandakoUrl) {
+    public void webguneakBilatu(String hitza) {
         ArrayList<Webgunea> results = new ArrayList<>();
         for (Webgunea webgunea : wLista) {
-            if (webgunea.getUrl().contains(emandakoUrl)) {
+            if (webgunea.getUrl().contains(hitza)) {
                 results.add(webgunea);
             }
         }
-
         results = (ArrayList<Webgunea>) webOrdenatua(results);
-
         if (results.isEmpty()) {
-            Menua.eragiketakBanatu();
             System.out.println("Ez da aurkitu bilatu nahi duzun web-orria.");
         } else {
-            Menua.eragiketakBanatu();
             System.out.println("Aurkitutako web-orriak (Alfabetikoki ordenatuak):");
             System.out.println();
             for (Webgunea webgunea : results) {
@@ -125,32 +120,24 @@ public class Nabigatzailea{
     public void webguneaBilatuId(String id) {
         for (Webgunea webgunea : wLista) {
             if (webgunea.getId().equals(id)) {
-                Menua.eragiketakBanatu();
                 System.out.println("ID hori duen web-orria:");
                 System.out.println();
                 System.out.println("ID:" + webgunea.getId() + "   " + " URL:" + webgunea.getUrl());
                 return;
             }
         }
-        Menua.eragiketakBanatu();
         System.out.println("Ez da aurkitu bilatu nahi duzun web-orria, edo zenbakia handiegia da.");
     }
     
-    
-    
     public void webguneaTxertatu(String url, String id) {
-        
         for (Webgunea webgunea : wLista){ 
             if (webgunea.getId().equals(id)) {
-                Menua.eragiketakBanatu();
                 System.out.println("Id hori best webgune batek erabiltzen du (URL:" + (webgunea.getUrl()) + "). Webgune hori ezabatu edo 2039804 baino balio handiagoa duen IDa sartu.");
                 return;
             }
         }
-
         Webgunea webgunea = new Webgunea(url, id);
         wLista.add(webgunea);   
-        Menua.eragiketakBanatu();
         System.out.println("Web-orria txertatu da. (ID: " + (webgunea.getId()) + ")");
     }
     
@@ -158,12 +145,10 @@ public class Nabigatzailea{
         for (Webgunea webgunea : wLista) {
             if (webgunea.getId().equals(id)) {
                 wLista.remove(webgunea);
-                Menua.eragiketakBanatu();
                 System.out.println("Web-orria ezabatu da. (URL: " + (webgunea.getUrl()) + ")");
                 return;
             }
         }
-        Menua.eragiketakBanatu();
         System.out.println("Ez da aurkitu ezabatu nahi duzun web-orria.");
     }
     
@@ -172,24 +157,20 @@ public class Nabigatzailea{
         for (Webgunea webgunea : wLista) {
             if (webgunea.getUrl().equals(url)) {
                 wLista.remove(webgunea);
-                Menua.eragiketakBanatu();
                 System.out.println("Web-orria ezabatu da. (ID: " + (webgunea.getId()) + ")");
                 return;
             }
         }
-        Menua.eragiketakBanatu();
         System.out.println("Ez da aurkitu ezabatu nahi duzun web-orria.");
     }
 
-    public void webguneLotu(String url, String lotuUrl) {
+    public void kanpoWebEstekatu(String url, String lotuUrl) {
         Webgunea webgunea = getWebguneaByUrl(url);
         Webgunea lotuWebgunea = getWebguneaByUrl(lotuUrl);
         if (webgunea != null && lotuWebgunea != null) {
             webgunea.getWebguneZerrenda().add(lotuWebgunea);
-            Menua.eragiketakBanatu();
             System.out.println("Web-orria lotu da.");
         } else {
-            Menua.eragiketakBanatu();
             System.out.println("Ez da aurkitu lotu nahi duzun web-orria.");
         }
     }
@@ -202,38 +183,5 @@ public class Nabigatzailea{
         }
         return null;
     }
-
-
-
-
-
-    /*public void gordailuIndex() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("index-2024-25"))) {
-            for (Webgunea webgunea : wLista) {
-                bw.write(webgunea.getId() + " ::: " + webgunea.getUrl());
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            Menua.eragiketakBanatu();
-            System.err.println("Errore bat artxiboa gordailuan:" + e.getMessage());
-        }
-    }
-
-    public void gordailuLoturikoak() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("pld-arcs-1-N-2024-25"))) {
-            for (Webgunea webgunea : wLista) {
-                bw.write(webgunea.getId() + " >>>> ");
-                for (Webgunea lotutakoWebgunea : webgunea.getWebguneZerrenda()) {
-                    bw.write(lotutakoWebgunea.getId() + " ### ");
-                }
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            Menua.eragiketakBanatu();
-            System.err.println("Errore bat artxiboa gordailuan: " + e.getMessage());
-        }
-    }*/
-    
-
 }
 
